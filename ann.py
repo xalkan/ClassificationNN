d194dd08f427dfb5612d0330a10517e97118b85d
xalkan <shahidalyk@gmail.com>
2018-09-21 01:35:06 +0500

    preprocessing updates

--- ann.py
+++ ann.py
@@ -7,11 +7,21 @@ import pandas as pd
 
 # import dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, [2,3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data and avoiding the dummy variable trap
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features= [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
 
 # Splitting dataset into the training and test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
